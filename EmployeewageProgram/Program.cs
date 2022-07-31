3989a9399a831ef72fc3f1ca69bb3f84adece9e0
Aman <amankr97111@gmail.com>
2022-07-31 14:06:03 +0530

    first commit

---
+++ EmployeewageProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int part_time = 1;
+            int full_time = 2;
+            int empcheck;
+
+            Random random = new Random();
+            empcheck = random.Next(0, 3);
+
+            if (empcheck == 0)
+            {
+                Console.WriteLine("Employe is absent");
+            }
+            else if (empcheck == 1)
+            {
+                Console.WriteLine("Employee is working part time");
+            }
+            else
+                    {
+                Console.WriteLine("Employee is working full time ");
+            }
+        }
+    }
+}
